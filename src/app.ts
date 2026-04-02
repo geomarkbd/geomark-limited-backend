74f81ea0c1a4e52f6e14c1ec3e31e6353d56591d
alaminarif <arifur3429@gmail.com>
2026-04-02 17:23:38 +0600

    sidebar added icon done

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to Tour Management System Backend",
+    message: "Welcome to Geomark limited System Backend",
   });
 });
 
